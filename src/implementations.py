5338f73658378ec89e0a5f840c028bb42e580151
Nicola Ischia <nicolaischia95@gmail.com>
2018-12-03 17:38:51 +0100

    pulizie primaverili

--- src/implementations.py
+++
@@ -1,8 +0,0 @@
-def pick_test_images():
-    test_imgs = []
-    for i in range(1, 51):
-        name = '../Data/test_set_images/test_'+str(i)+'/test_' + str(i) + '.png'
-        test_imgs.append(load_image(name))
-    return test_imgs
-        
-test = pick_test_images()
